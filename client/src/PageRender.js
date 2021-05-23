62aace6ad3decfb84120af83716239a64eeef974
EnkhAmar <enkhamar1220@gmail.com>
2021-05-23 12:38:16 +0800

    #frontend: Initial commit

---
+++ client/src/PageRender.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { generatePath, useParams } from 'react-router-dom'
+import NotFound from './components/NotFound'
+
+const generatePage = (pageName) => {
+    const component = () => require(`./pages/${pageName}`).default
+
+    try {
+        return React.createElement(component())
+    } catch (error) {
+        return <NotFound />
+    }
+}
+
+function PageRender() {
+    const { page, id } = useParams()
+    let pageName = ""
+    if (id) {
+        pageName = `${page}/[id]`
+    } else {
+        pageName = `${page}`
+    }
+    console.log(pageName)
+    return generatePage(pageName)
+}
+
+export default PageRender
