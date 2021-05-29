52e926ee9945d8ae48de8bc7e6fe7560ec8ff74d
Enkh-Amar <enkhamar1220@gmail.com>
2021-05-29 13:28:13 +0800

    #backen: Fixed some bugs related to PUT request

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,7 +7,7 @@ router.get('/search', auth, userCtrl.searchUser)
 
 router.get('/user/:id', auth, userCtrl.getUser)
 
-router.patch('/user', auth, userCtrl.updateUser)
+router.put('/user', auth, userCtrl.updateUser)
 
 
 module.exports = router
