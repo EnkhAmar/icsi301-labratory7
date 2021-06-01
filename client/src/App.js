46c4a53f5eb580192ff0f0cdbec8a35cef020417
EnkhAmar <enkhamar1220@gmail.com>
2021-06-01 12:22:00 +0800

    #frontend: Status UI

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,13 @@ import Register from './pages/register'
 
 import Alert from './components/alert/Alert'
 import Header from './components/header/Header'
+import StatusModal from './components/StatusModal'
 
 import { useSelector, useDispatch } from 'react-redux'
 import { refreshToken } from './redux/actions/authAction'
 
 function App() {
-  const { auth } = useSelector(state => state)
+  const { auth, status } = useSelector(state => state)
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -29,6 +30,8 @@ function App() {
       <div className="App">
         <div className="main">
           {auth.token && <Header />}
+          {status && <StatusModal />}
+          
           <Route exact path="/" component={auth.token ? Home : Login} />
           <Route exact path="/register" component={Register} />
 
