9c663c729b8f0d3e4f9baf7d17ea9b6858a60b9a
EnkhAmar <enkhamar1220@gmail.com>
2021-06-06 11:49:35 +0800

    #backend: Edit comment & like, unlike comment

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -4,6 +4,12 @@ const auth = require('../middleware/auth')
 
 router.post('/comment', auth, commentCtrl.createComment)
 
+router.put('/comment/:id', auth, commentCtrl.updateComment)
+
+router.put('/comment/:id/like', auth, commentCtrl.likeComment)
+
+router.put('/comment/:id/unlike', auth, commentCtrl.unLikeComment)
+
 
 
 module.exports = router
